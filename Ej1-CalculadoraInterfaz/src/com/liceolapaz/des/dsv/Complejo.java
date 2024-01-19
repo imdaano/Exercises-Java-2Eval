e027acbea510194298cb642369c12b1765bfe664
imdaano <danisanchezpsc@gmail.com>
2024-01-19 09:48:31 +0100

    FEAT add new code to complejo class

--- Ej1-CalculadoraInterfaz/src/com/liceolapaz/des/dsv/Complejo.java
+++ Ej1-CalculadoraInterfaz/src/com/liceolapaz/des/dsv/Complejo.java
@@ -1,4 +1,31 @@
 package com.liceolapaz.des.dsv;
 
-public class Complejo {
+public class Complejo extends Numero {
+
+
+
+    @Override
+    protected Numero suma(Numero operando) {
+        return null;
+    }
+
+    @Override
+    protected Numero resta(Numero operando) {
+        return null;
+    }
+
+    @Override
+    protected Numero producto(Numero operando) {
+        return null;
+    }
+
+    @Override
+    protected Numero division(Numero operando) {
+        return null;
+    }
+
+    @Override
+    protected String mostrar() {
+        return null;
+    }
 }
